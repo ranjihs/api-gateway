fd4361c0812bbd444814d928bdd9a6004365a743
rhoshino <rhoshino@insside.net>
2021-07-03 13:22:33 -0300

    api-gateway first commit

---
+++ api-gateway/src/test/java/com/ranji/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ranji.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
